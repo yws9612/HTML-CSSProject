77a8d2710339adf479a94b26cbae00e1544b71e1
nonameleel <xcvm92@gmail.com>
2022-07-16 20:31:00 +0900

    슬라이드조정_송이_0716

--- slider.js
+++ slider.js
@@ -1,65 +1,76 @@
-$(document).ready(function() {
-    var getProductHeight = $('.product.active').height();
-  
+$(document).ready(function () {
+  var getProductHeight = $('.product.active').height();
+
+  $('.products').css({
+    height: getProductHeight
+  });
+
+  function calcProductHeight() {
+    getProductHeight = $('.product.active').height();
+
     $('.products').css({
       height: getProductHeight
     });
+  }
+
   
+
+  var productItem = $('.product'),
+    productCurrentItem = productItem.filter('.active');
     
-    
-    var productItem = $('.product'),
-      productCurrentItem = productItem.filter('.active');
-  
-    $('#next').on('click', function(e) {
-      e.preventDefault();
-  
-      var nextItem = productCurrentItem.next();
-  
-      productCurrentItem.removeClass('active');
-  
-      if (nextItem.length) {
-  
-        productCurrentItem = nextItem.addClass('active');
-      } else {
-        productCurrentItem = productItem.first().addClass('active');
-      }
-  
-      
-    });
-  
-    $('#prev').on('click', function(e) {
-      e.preventDefault();
-  
-      var prevItem = productCurrentItem.prev();
-  
-      productCurrentItem.removeClass('active');
-  
-      if (prevItem.length) {
-        productCurrentItem = prevItem.addClass('active');
-      } else {
-        productCurrentItem = productItem.last().addClass('active');
-      }
-  
-     
-    });
-  
-    // Ripple
-    $('[ripple]').on('click', function(e) {
-      var rippleDiv = $('<div class="ripple" />'),
-        rippleSize = 60,
-        rippleOffset = $(this).offset(),
-        rippleY = e.pageY - rippleOffset.top,
-        rippleX = e.pageX - rippleOffset.left,
-        ripple = $('.ripple');
-  
-      rippleDiv.css({
-        top: rippleY - (rippleSize / 2),
-        left: rippleX - (rippleSize / 2),
-        background: $(this).attr("ripple-color")
-      }).appendTo($(this));
-  
-      window.setTimeout(function() {
-        rippleDiv.remove();
-      }, 1900);
-    });
+
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+
+    var nextItem = productCurrentItem.next();
+
+    productCurrentItem.removeClass('active');
+
+    if (nextItem.length) {
+
+      productCurrentItem = nextItem.addClass('active');
+    } else {
+      productCurrentItem = productItem.first().addClass('active');
+    }
+
+    calcProductHeight();
+
   });
+
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+
+    var prevItem = productCurrentItem.prev();
+
+    productCurrentItem.removeClass('active');
+
+    if (prevItem.length) {
+      productCurrentItem = prevItem.addClass('active');
+    } else {
+      productCurrentItem = productItem.last().addClass('active');
+    }
+
+    calcProductHeight();
+
+  });
+
+  // Ripple
+  $('[ripple]').on('click', function (e) {
+    var rippleDiv = $('<div class="ripple" />'),
+      rippleSize = 60,
+      rippleOffset = $(this).offset(),
+      rippleY = e.pageY - rippleOffset.top,
+      rippleX = e.pageX - rippleOffset.left,
+      ripple = $('.ripple');
+
+    rippleDiv.css({
+      top: rippleY - (rippleSize / 2),
+      left: rippleX - (rippleSize / 2),
+      background: $(this).attr("ripple-color")
+    }).appendTo($(this));
+
+    window.setTimeout(function () {
+      rippleDiv.remove();
+    }, 1900);
+  });
+});
